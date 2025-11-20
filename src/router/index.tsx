9fe19a93c671294fcd1417e8d23b22c19409072a
junhyeon0218 <junhyeon0218@gmail.com>
2025-11-21 02:43:25 +0900

    router 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom';
-import DashboardPage from '../pages/DashboardPage';
-import TransactionsPage from '../pages/TransactionsPage';
 import MainLayout from '../components/layout/MainLayout';
+import Dashboard from '../components/dashboard/Dashboard';
+import TransactionsPage from '../pages/TransactionsPage';
+import MerchantsPage from '../pages/MerchantsPage';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <DashboardPage />,
-      },
-      {
-        path: 'transactions',
-        element: <TransactionsPage />,
-      },
-    ],
-  },
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />,
+            },
+            {
+                path: 'transactions',
+                element: <TransactionsPage />,
+            },
+            {
+                path: 'merchants',
+                element: <MerchantsPage />,
+            },
+        ],
+    },
 ]);
 
 export default router;
