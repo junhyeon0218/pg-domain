0882b4ee684f543a1df4c5b973dfc3a5c32cc80e
junhyeon0218 <junhyeon0218@gmail.com>
2025-11-21 02:39:42 +0900

    프리티어 수정으로 인한 변경 및 common.ts에 payme'm't 오타수정

--- src/apis/api/common.ts
+++ src/apis/api/common.ts
@@ -3,18 +3,18 @@ import instance from '../util/instance';
 
 // 결제 상태 코드 조회
 export const getPaymentStatus = async (): Promise<ApiResponse<CommonCode[]>> => {
-  const response = await instance.get('/common/payment-status/all');
-  return response.data;
+    const response = await instance.get('/common/payment-status/all');
+    return response.data;
 };
 
-// 결제 수단 코드 조회
+// 결제 수단 코드 조회 (payment x -> paymemt o 오타가 있는듯 하다)
 export const getPaymentType = async (): Promise<ApiResponse<PaymentType[]>> => {
-  const response = await instance.get('/common/payment-type/all');
-  return response.data;
+    const response = await instance.get('/common/paymemt-type/all');
+    return response.data;
 };
 
 // 가맹점 상태 코드 조회
 export const getMchtStatus = async (): Promise<ApiResponse<CommonCode[]>> => {
-  const response = await instance.get('/common/mcht-status/all');
-  return response.data;
+    const response = await instance.get('/common/mcht-status/all');
+    return response.data;
 };
--- src/apis/api/payments.ts
+++ src/apis/api/payments.ts
@@ -3,6 +3,6 @@ import instance from '../util/instance';
 
 // 거래 내역 전체 조회
 export const getPaymentsList = async (): Promise<ApiResponse<Payment[]>> => {
-  const response = await instance.get('/payments/list');
-  return response.data;
+    const response = await instance.get('/payments/list');
+    return response.data;
 };
